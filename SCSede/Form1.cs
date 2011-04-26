a9f023a2f728ba588cab22bf18436f0e248b8b12
Noah Callaway <ncallaway@gmail.com>
2011-04-26 00:56:56 -0700

    SCSede

---
+++ SCSede/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SCSede
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stream stream = File.Open("Stages.bin", FileMode.Create);
+            BinaryFormatter binary = new BinaryFormatter();
+
+            binary.Serialize(stream, stageManager);
+            stream.Close();
+        }
+    }
+}
