ad7895591aa434059b15b660fcad7e525331692b
Noah Callaway <ncallaway@gmail.com>
2011-05-08 19:24:03 -0700

    Falsify save/load

--- StoneCircle/GameManager.cs
+++ StoneCircle/GameManager.cs
@@ -121,7 +121,7 @@ namespace StoneCircle
                 }
             }
 
-            if (Player.Input.IsLeftBumperNewlyPressed())
+            if (Player.Input.IsLeftBumperNewlyPressed() && false)
             {
 #if XBOX
                 /* Request the container */
@@ -135,7 +135,7 @@ namespace StoneCircle
                 
             }
 
-            if (Player.Input.IsRightBumperNewlyPressed())
+            if (Player.Input.IsRightBumperNewlyPressed() && false)
             {
 #if XBOX
                 loadRequested = true;
